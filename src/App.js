c5335b60058d06196f3469faaa6cb96c593a4380
minh22091996 <tranbinhminh22091996@gmail.com>
2023-02-07 16:36:08 +0700

    crud-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from "axios"
 import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 function App() {
   const [list, setList] = useState([]);
   const [title, setTitle] = useState("");
   const [age, setAge] = useState("")
   const link = "http://localhost:3000/room"
+  const [nameValue, setNameValue] = useState("");
+  const [ageValue, setAgeValue] = useState("");
+  const [idUpdate,setIdUpdate]  = useState('');
+  const [valuePayment,setValuePayment]  = useState('');
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+
   useEffect(() => {
     getUserPermissions();
   }, [])
@@ -23,69 +33,150 @@ function App() {
     } else {
       axios.post(link, {
         name: `${title}`,
-        age: `${age}`
+        age: `${age}`,
+        payment:"unpaid"
       }).then(() => {
         getUserPermissions();
         setTitle("");
-        setAge("   ");
+        setAge("");
       })
     }
   }
 
   async function deleteList(id) {
-    console.log(id);
-    const response = await axios.delete(`${link}/${id}`);
-    console.log("43434", response);
+   await axios.delete(`${link}/${id}`);
     getUserPermissions();
-    return response;
+   
   }
 
-
-  function editList() {
-    console.log("zz");
+  function editList(id) {
+    setShow(true);
+      for (let i = 0; i < list.length; i++) {
+        if (list[i].id == id) {
+          setNameValue(list[i].name);
+          setAgeValue(list[i].age);
+          setIdUpdate(id);
+          setValuePayment(list[i].payment)
+        }
+      }
   }
 
-  function finishList() {
-    console.log("zz");
+  function changePayment(idChangePayment) {
+    console.log("444444",idChangePayment);
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].id == idChangePayment) {
+            if(list[i].payment=="unpaid"){
+              axios.put(`${link}/${idChangePayment}`, {
+                name: `${list[i].name}`,
+                age: `${list[i].age}`,
+                payment:"paid"
+              })
+              .then(() => {
+                getUserPermissions();
+               
+              })
+            }else{
+              axios.put(`${link}/${idChangePayment}`, {
+                name: `${list[i].name}`,
+                age: `${list[i].age}`,
+                payment:"unpaid"
+              })
+              .then(() => {
+                getUserPermissions();     
+              })
+            }
+      }
+    }
+  }
+  async function handleCloseTab(idUpdate){
+    console.log("5454545",idUpdate);
+    axios.put(`${link}/${idUpdate}`, {
+      name: `${nameValue}`,
+      age: `${ageValue}`,
+      payment:`${valuePayment}`
+    })
+    .then(() => {
+      getUserPermissions();
+     
+    })
+    handleClose();
   }
 
   return (
-
     <div className='container'>
       <h1> DANH SACH SINH VIEN</h1>
       <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="nhập tên" />
       <input value={age} onChange={(e) => setAge(e.target.value)} placeholder="nhập tuổi" />
       <button className='post' onClick={postList}>post</button>
 
-      <div className='return'>
+      <div className='return' style={{margin:" auto",marginTop:"20px"}}>
 
         <table className='table'>
+          <thead>
           <tr className='tr'>
             <th className='th'>stt</th>
-            <th className='th'>ten sinh vien</th>
-            <th className='th'>tuoi sinh vien</th>
-            <th className='th'>trang thai</th>
-            <th className='th'>chuc nang</th>
+            <th className='th'>tên sinh viên </th>
+            <th className='th'>tuổi sinh viên </th>
+            <th className='th'>thanh toán</th>
+            <th className='th'> chức năng </th>
           </tr>
+          </thead>
           {
             list.map((e, i) => {
               return (
-                <tr className='tr' key={e.id}>
+                <tbody key={e.id}>
+                  <tr className='tr' >
                   <td className='td'>{e.id}</td>
                   <td className='td'>{e.name}</td>
                   <td className='td'>{e.age}</td>
-                  {/* <td className='td'>{e.status}</td> */}
+                  <td className='td'>{e.payment}</td>
                   <td className='td'><button onClick={() => {
                     deleteList(e.id)
                   }} className='delete'>delete</button>
-                    <button className="edit" onClick={editList} >edit</button>
-                    <button className="finish" onClick={finishList} >finish</button></td>
+                    <button className="edit" onClick={() => {
+                      editList(e.id)
+                    }} >edit</button>
+                    <button className="finish" onClick={()=>{
+                      changePayment(e.id)
+                    }} >finish</button></td>
                 </tr>
+                </tbody>
               )
             })
           }
         </table>
       </div>
+
+      {/* Modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input placeholder='nhập tên'
+            value={nameValue}
+            onChange={(e) =>
+              setNameValue(e.target.value)
+            }
+          >
+          </input>  <br /><br />
+          <input placeholder='nhập tuổi' value={ageValue}
+            onChange={(e) => {
+              setAgeValue(e.target.value)
+            }}></input>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={()=>{
+            handleCloseTab(idUpdate)
+          }}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
